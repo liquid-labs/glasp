81e0008e28cf420d7e9a293990c32acb1b57d1ff
Zane Rockenbaugh <zane@liquid-labs.com>
2018-03-26 13:07:16 -0500

    more checks and refinements based on the clasp action

--- glasp.sh
+++ glasp.sh
@@ -7,19 +7,34 @@ for COM in clasp git; do
         { echo "Requierd command $COM not found. Aborting." >&2; exit 1; }
 done
 
-if [ ! -f .clasp.json ]; then
-    echo "Did not find '.clasp.json' file. Execute from clasp root directory." >&2
-    exit 1
-fi
+# Check re. actions using .clasp.json
+case "$ACTION" in
+  pull|push|open|deployments|deploy|undeploy|redeploy|versions|version)
+    if [ ! -f .clasp.json ]; then
+      echo "Did not find '.clasp.json' file. Execute from clasp root directory." >&2
+      exit 1
+    fi;;
+  create|clone)
+    if [ -f .clasp.json ]; then
+      echo "Existing '.clasp.json' found. Aborting." >&2
+      exit 1
+    fi;;
+esac
 
-if [ x"$ACTION" == x"pull" ]; then
-   cp "$PWD/.glaspignore" "$PWD/.claspignore"
-   # on pull, also ignore the library files, which are only pushed
-   echo >> "$PWD/.claspignore"
-   grep --no-filename -e '^#do-push-file' "$PWD/.glaspignore" | awk '{print $2}' >> "$PWD/.claspignore"
-else
-    # on all other actions, only ignore the commonly ignored files
-   cp "$PWD/.glaspignore" "$PWD/.claspignore"
+# Do we need to do something with .glaspignore
+if [ -f $PWD/.glaspignore ]; then
+  case "$ACTION" in
+    pull)
+      cp "$PWD/.glaspignore" "$PWD/.claspignore"
+      # on pull, also ignore the library files, which are only pushed
+      echo >> "$PWD/.claspignore"
+      grep --no-filename -e '^#do-push-file' "$PWD/.glaspignore" | awk '{print $2}' >> "$PWD/.claspignore"
+      ;;
+    push)
+      # on all other actions, only ignore the commonly ignored files
+      cp "$PWD/.glaspignore" "$PWD/.claspignore"
+      ;;
+  esac
 fi
 
 exec clasp "$@"
